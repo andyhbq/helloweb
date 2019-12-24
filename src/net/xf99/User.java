7574ecb3cba13f86d885792f4ce917e1fbd651c2
andywong <andywong@andywong-3.local>
2019-12-24 18:07:20 +0800

    Initial commit

---
+++ src/net/xf99/User.java
@@ -0,0 +1,13 @@
+package net.xf99;
+
+/**
+ * @author andywong
+ * @projectName helloweb
+ * @packageName net.xf99
+ * @createTime 2019-12-23 16:26
+ */
+public class User {
+	public static void main(String[] args) {
+		System.out.printf("11111");
+	}
+}
